ee5ba2d8cdcc5727891211352635569fe0eb291c
Areeb208 <areebk20012@gmai.com>
2026-01-18 21:44:44 +0500

    Changed the extensions

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,8 +1,8 @@
 import styles from "./index.module.css";
 
 import avocadoImg from "../../assets/proj-img/Avacado.png";
-import electromartImg from "../../assets/proj-img/Electro.png";
-import shadowlineImg from "../../assets/proj-img/ShadowLine.png";
+import electromartImg from "../../assets/proj-img/Electro.PNG";
+import shadowlineImg from "../../assets/proj-img/ShadowLine.PNG";
 
 const projects = [
   {
